d44688ee855c24496386b7ae0821b6fde0abd6eb
Emils <kepkepgithub@gmail.com>
2020-07-26 08:48:02 +0300

    Finished?

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var mySwiper = new Swiper('.swiper-container', {
     allowTouchMove: false,
     speed: 1000,
     autoplay: {
-        delay: 1000,
+        delay: 2000,
       },
     pagination: {
       el: '.swiper-pagination',
@@ -12,6 +12,22 @@ var mySwiper = new Swiper('.swiper-container', {
     }
   })
 
-  mySwiper.on('slideChange', function () {
+var slide_content = document.querySelectorAll(".slider_text_wrap span, .slider_text_wrap h1, .slide_image");
+window.onload = function(){
+  slide_content.forEach((e)=>{
+    e.classList.add("slide_animation");
+  })
+}
+  mySwiper.on('slideChangeTransitionStart', function () {
+    // when the slide changes hides all the elements
     console.log('slide changed');
+    slide_content.forEach((e)=>{
+      e.classList.remove("slide_animation");
+    })
+    // after a short timeout applies the animation (class)
+    setTimeout(function(){
+    slide_content.forEach((e)=>{
+      e.classList.add("slide_animation");
+    })
+    },640)
   });
