cd75453fa56cc630d47133609aa59cd40feb89a5
Emils <kepkepgithub@gmail.com>
2020-07-27 18:55:17 +0300

    SLIDES FIXED YEEHAW

--- script.js
+++ script.js
@@ -11,23 +11,42 @@ var mySwiper = new Swiper('.swiper-container', {
       clickable: true,
     }
   })
+  // when user opens page load the animations onto the first slide
+window.onload = ()=>{
+  const active_slide_image = document.querySelector(".swiper-slide-active .slide_image");
+  // "New product text"
+  const active_slide_text1 = document.querySelector(".swiper-slide-active .slider_content span");
+  // Product description text
+  const active_slide_text2 = document.querySelector(".swiper-slide-active .slider_content h1");
 
-var slide_content = document.querySelectorAll(".slider_text_wrap span, .slider_text_wrap h1, .slide_image");
-window.onload = function(){
-  slide_content.forEach((e)=>{
-    e.classList.add("slide_animation");
-  })
+  active_slide_image.classList.add("slide_animation");
+  active_slide_text1.classList.add("slide_animation");
+  active_slide_text2.classList.add("slide_animation");
 }
-  mySwiper.on('slideChangeTransitionStart', function () {
-    // when the slide changes hides all the elements
-    console.log('slide changed');
-    slide_content.forEach((e)=>{
+  mySwiper.on('slideChangeTransitionEnd', function () {
+    // Removes the class from all other slides 
+    const all_slides_image = document.querySelectorAll(".swiper-slide .slide_image");
+    const all_slides_text1 = document.querySelectorAll(".swiper-slide .slider_content span");
+    const all_slides_text2 = document.querySelectorAll(".swiper-slide .slider_content h1");
+    all_slides_image.forEach((e)=>{
       e.classList.remove("slide_animation");
     })
-    // after a short timeout applies the animation (class)
-    setTimeout(function(){
-    slide_content.forEach((e)=>{
-      e.classList.add("slide_animation");
+    all_slides_text1.forEach((e)=>{
+      e.classList.remove("slide_animation");
     })
-    },640)
+    all_slides_text2.forEach((e)=>{
+      e.classList.remove("slide_animation");
+    })
+    // adds the class to the active 
+    // QUERRIES NEED TO BE UPPDATED EVERY TIME THE SLIDE CHANGES THAT'S WHY THEY ARE HERE
+    const active_slide_image = document.querySelector(".swiper-slide-active .slide_image");
+    // "New product text"
+    const active_slide_text1 = document.querySelector(".swiper-slide-active .slider_content span");
+    // Product description text
+    const active_slide_text2 = document.querySelector(".swiper-slide-active .slider_content h1");
+
+    console.log('slide changed');
+    active_slide_image.classList.add("slide_animation");
+    active_slide_text1.classList.add("slide_animation");
+    active_slide_text2.classList.add("slide_animation");
   });
